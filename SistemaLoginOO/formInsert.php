99908ec88b7f7f55e70587550e1d0948b3eeec4c
daniale5 <115493195+daniale5@users.noreply.github.com>
2024-04-18 14:05:05 -0300

    Add files via upload

---
+++ SistemaLoginOO/formInsert.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title> Insert </title>
+</head>
+<body>
+    <header> Cadastrar dados dos clientes </header>
+    <section>
+       <form action="insert.php" method="post">
+        <p> Nome: <input type="text" name="nome"> </p>
+        <p>E-mail: <input type="email" name="e-mail"></p>
+        <p>Idade: <input type="number" name="idade"></p>
+
+        <button type="submit"> Inserir dados </button>
+       </form>
+    </section>
+</body>
+</html>
+
+
